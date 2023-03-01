0c055e9090291e860b6127b15c8baa51bb05c854
Stefania <picioreastefania@yahoo.com>
2023-03-01 06:56:25 -0500

    Finished adding the event listeners on the notifications and the Mark all Read span
    - Finished adding the event listeners that trigger the red dot to disappear when the notification is clicked on.
    
    - Also added the event listener on the Mark All Read span that triggers the style change for all notifications at once when clicked.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
 
-let unreadMessages = document.querySelectorAll('.unread'); 
+// let unreadMessages = document.querySelectorAll('.unread'); 
 
-unreadMessages.forEach(message => { 
+// unreadMessages.forEach(message => { 
 
-     message.addEventListener('click', (event) => { 
+//      message.addEventListener('click', (event) => { 
         
-           let parentDiv = event.target.closest('.unread'); 
+//            let parentDiv = event.target.closest('.unread'); 
 
-           if (parentDiv) {     
+//            if (parentDiv) {     
             
-                parentDiv.style.backgroundColor = 'white'; 
+//                 parentDiv.style.backgroundColor = 'white'; 
                 
-           }
-       });
- }); 
+//            }
+//        });
+//  }); 
 
+const notifications = document.querySelectorAll('.unread')
+
+notifications.forEach(notification => {
+     notification.addEventListener('click', () => {
+          notification.style.background = '#fff'
+          const badge = notification.querySelector('.badge')
+          badge.style.display = 'none'
+
+     })
+})
 
 //  document.getElementsByClassName('badge').style.display = 'none'  TRY THIS AGAIN TOMORROW
 
 
+const markAll = document.querySelector('.markRead')
 
+markAll.addEventListener('click', () => {
+     const notifications = document.querySelectorAll('.unread')
+     notifications.forEach(notification => {
+          notification.style.background = '#fff'
+          const badge = notification.querySelector('.badge')
+          badge.style.display = 'none'
+     })
+})
